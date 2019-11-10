0e78ebf435bf635bb592396278969e160c0d60dc
Glitch (fragrant-nectarine) <none>
2019-11-10 18:53:42 +0000

    🌽🚪 used alert and prompt for the first time.

--- js/external.js
+++ js/external.js
@@ -1,1 +1,7 @@
 console.log("hello from external javaScript");
+
+alert("Welcome to my website!");
+
+var userInput = prompt("what is your favorite color?");
+console.log ("the user entered: " + userInput );
+alert(userInput + " is also my favorite color! " );
